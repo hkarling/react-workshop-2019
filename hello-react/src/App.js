179f609b33e0a3787c48a8426ec0f04247c91e3c
hkarling <dantert@gmail.com>
2019-09-04 13:17:45 +0900

    defaultProps의 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   }
   render() {
     return (
-      <MyComponent name="리엑트" />
+      <MyComponent />
     );
   }
 }
--- hello-react/src/components/MyComponent.js
+++ hello-react/src/components/MyComponent.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 class MyComponent extends Component {
-    render() { 
+    // transform-class-properties 문법
+    static defaultProps = {
+        name: '기본'
+    }
+    render() {
         return ( // props로 넘어오는 값을 받는다.
-            <div> 
-                안녕하슈, 제 이름은 <b>{this.props.name}</b>입니다. 
+            <div>
+                안녕하슈, 제 이름은 <b>{this.props.name}</b>입니다.
             </div>
         );
     }
+}
+
+// defaultProps 기본값 설정
+MyComponent.defaultProps = {
+    name: '기본이름'
 }
 
 export default MyComponent;
